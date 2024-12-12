cf98848e27c3c4ed27b27d44334005656cbd0969
Smae Tongmenglay <smaetongmenglay@MENGLAY.local>
2024-12-13 02:40:27 +0700

    ...

--- routes/api.php
+++ routes/api.php
@@ -1,15 +1,15 @@
 <?php
 
-use App\Http\Controllers\Api\V1\UserController;
-use App\Http\Controllers\Api\V1\OtpController;
+use App\Http\Controllers\Api\V1\GmailController;
+use App\Http\Controllers\Api\V1\RegisterController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::apiResource('/users', UserController::class);
-
-Route::post('/otp', [OtpController::class, 'sendOtp']);
-Route::post('/verify-otp', [OtpController::class, 'verifyOtp']);
-
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::prefix('v1')->group(function () {
+    Route::post('/send-email', [GmailController::class, 'sendEmail']);
+    Route::post('/register', [RegisterController::class, 'register']);
+    Route::post('/complete-registration', [RegisterController::class, 'completeRegistration']);
+    Route::get('/user', function (Request $request) {
+        return $request->user();
+    })->middleware('auth:sanctum');
+});
